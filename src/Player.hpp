2e1895295da0f8c2d88a7263fd6022595b0f933f
janniks mac <jannik.loth@gmx.de>
2023-06-24 09:59:20 +0200

    basic Player()

--- src/Player.hpp
+++ src/Player.hpp
@@ -6,9 +6,29 @@
 #define CONNECT4_CONSOLEGAME_PLAYER_HPP
 
 
+#include "Board.hpp"
+
+
 class Player
 {
+public:
+    // default constructor
+    Player()
+    {};
 
+    // overloaded constructor
+    Player(std::string name, FieldColor color)
+        : m_name(name)
+        , m_color(color)
+    {};
+
+    // destructor
+    ~Player()
+    {};
+
+private:
+    std::string m_name;
+    FieldColor m_color;
 };
 
 
